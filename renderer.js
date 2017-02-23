fc11698362c80c1e63336cd8d490e24a174801af
Edvard A. M. Haaland <fishurban@gmail.com>
2017-02-23 02:03:12 +0100

    Fixed wrapper + some symbols + changed readme

--- renderer.js
+++ renderer.js
@@ -6,7 +6,7 @@ function SubmitText() {
 	var inputText = document.getElementById('inputText').value.toLowerCase();
 	var inputLength = inputText.length;
 	if (!inputText) {
-		alert('There is nothing in the input field!\n Do not let me go hungry, now!');
+		alert('There is nothing in the input field!\nDo not let me go hungry, now!');
 		return;
 	}
 	else {
@@ -36,10 +36,49 @@ function SubmitText() {
 			}
 			else if (letter === ",") {
 				document.getElementById('outputText').innerHTML += ":small_blue_diamond: ";
-			}
-			else if (letter != " "||"*"||"?"||"!") {
+			}/*
+			else if (letter === "1"||"2"||"3"||"4"||"5"||"6"||"7"||"8"||"9"||"0") {
+				console.log('hi');
+				var newLetter = NumberToString(letter);
+				document.getElementById('outputText').innerHTML += ":"+newLetter+": ";
+			}*/
+			else if (letter != " "||"*"||"?"||"!"||"'"||"."||","/*||"1"||"2"||"3"||"4"||"5"||"6"||"7"||"8"||"9"||"0"*/) {
 				document.getElementById('outputText').innerHTML += ":regional_indicator_"+letter+": ";
 			}
 		}
 	}
 }
+/*
+var NumberToString = function (number) {
+	if (number === '1') {
+		return 'one';
+	}
+	else if (number === '2') {
+		return 'two';
+	}
+	else if (number === '3') {
+		return 'three';
+	}
+	else if (number === '4') {
+		return 'four';
+	}
+	else if (number === '5') {
+		return 'five';
+	}
+	else if (number === '6') {
+		return 'six';
+	}
+	else if (number === '7') {
+		return 'seven';
+	}
+	else if (number === '8') {
+		return 'eight';
+	}
+	else if (number === '9') {
+		return 'nine';
+	}
+	else if (number === '0') {
+		return 'zero';
+	}
+}
+*/
