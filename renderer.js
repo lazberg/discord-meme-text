d7b1cadacb0a203f261ed86c119fab65027fab3a
Edvard A. M. Haaland <fishurban@gmail.com>
2017-02-23 01:10:28 +0100

    Woomp there is it
    Annoy the hell out of your pals!

---
+++ renderer.js
@@ -0,0 +1,36 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+function SubmitText() {
+	console.log('Submitted!');
+	var inputText = document.getElementById('inputText').value.toLowerCase();
+	var inputLength = inputText.length;
+	if (!inputText) {
+		alert('There is nothing in the input field!\n Do not let me go hungry, now!');
+		return;
+	}
+	else {
+		console.log('Text vored');
+		document.getElementById('outputText').innerHTML = "";
+		document.getElementById('outputText').placeholder = "calculating..";
+		for (var i=0;i<inputLength;i++) {
+			console.log('letter ' + (i+1));
+			var letter = inputText.charAt(i);
+			if (letter === " ") {
+				document.getElementById('outputText').innerHTML += ":fire: ";
+			}
+			else if (letter === "*") {
+				document.getElementById('outputText').innerHTML += ":asterisk: ";
+			}
+			else if (letter === "?") {
+				document.getElementById('outputText').innerHTML += ":question: ";
+			}
+			else if (letter === "!") {
+				document.getElementById('outputText').innerHTML += ":exclamation: ";
+			}
+			else if (letter != " "||"*"||"?"||"!") {
+				document.getElementById('outputText').innerHTML += ":regional_indicator_"+letter+": ";
+			}
+		}
+	}
+}
